381da1380880329638846dda1e7cd49d6cc541c6
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-07-29 09:26:39 +0530

    feat: font-awesome
    font-awesome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import ProductCard from "../components/ProductCard";
+
 export default function Home(){
     return(
         <div>
             <h1>Welcome to Homepage MCRSession-nine. 
                 I am Awesome! :P 
             </h1>
+            <ProductCard/>
         </div>
     )
 }
