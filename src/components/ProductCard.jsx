283dc22df84429ff695b1caa3b972e9bb945be49
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-07-29 09:43:00 +0530

    fix: font-awesome
    font-awesome

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { background } from "@chakra-ui/react";
 import "../card2.css";
 import "font-awesome/css/font-awesome.min.css";
 
@@ -5,7 +6,7 @@ export default function ProductCard() {
   return (
     <div class="cardtwo">
       <div class="wishlist-icon">
-        <i class="fas fa-heart"></i>
+        <i class="fa fa-heart"></i>
       </div>
       <img
         src="https://lh3.googleusercontent.com/hwau7OVWx96XaME5KpRuJ0I_MscrerK6SbRH1UwYHYaxIDQQtn7RZK02LDSfBzCreidFgDsJeXyqDct6EZiH6vsV=w640-h400-e365-rj-sc0x00ffffff"
